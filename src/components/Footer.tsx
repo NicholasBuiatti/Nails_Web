a46679101bfcf3e9003a5a1f37f3945e22a63907
Nicholas Buiatti <160488490+NicholasBuiatti@users.noreply.github.com>
2025-09-24 15:08:05 +0200

    uso di container

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,7 +17,7 @@ const AppFooter = () => {
           <svg className="fill-gray-300 hover:fill-gray-400 transition-colors" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><g data-name="90-Arrow Up"><path d="M16 0a16 16 0 1 0 16 16A16 16 0 0 0 16 0zm0 30a14 14 0 1 1 14-14 14 14 0 0 1-14 14z" /><path d="m15.29 10.29-8 8L8.7 19.7l7.3-7.29 7.29 7.29 1.41-1.41-8-8a1 1 0 0 0-1.41 0z" /></g></svg>
         </button>
       }
-      <div className="max-w-6xl mx-auto">
+      <div className="container mx-auto">
         <div className="flex text-gray-400">
           <div className="w-full md:w-4/12 text-center md:text-start">
             <p>2025 Raffaella Deliso</p>
